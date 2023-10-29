6743aafcf0cbb9ba2857911935e2cb6b18b75e5f
talal-098 <talal.qayyum@creditbook.pk>
2023-10-29 17:20:10 +0500

    Removed console log

--- src/controllers/getPageRank.ts
+++ src/controllers/getPageRank.ts
@@ -3,7 +3,6 @@ import graph from "../data";
 export async function getPageRank(req, res) {
   try {
     const pageRank = graph.calculatePageRank();
-    console.log(pageRank);
     res.status(200).send(pageRank);
   } catch (error) {
     res.status(404).json({
